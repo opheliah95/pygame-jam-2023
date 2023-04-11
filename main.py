a1dbcfed4a88dd9b4a7692b2bab3bc3097bba899
opheliah95 <62559892+opheliah95@users.noreply.github.com>
2023-04-11 21:02:10 +0100

    feat: added color

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import pygame
 from pygame.locals import *
- 
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (3, 41, 63)
+FPS = 60
+
 class App:
     def __init__(self) -> None:
         self._running = True
         self._display_surf = None
         self.size = self.weight, self.height = 640, 400
+        self.clock = pygame.time.Clock()
+
     def on_init(self):
         pygame.init()
-        self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        self._display_surf = pygame.display.set_mode(
+            self.size, pygame.HWSURFACE | pygame.DOUBLEBUF
+        )
         self._running = True
 
     def on_event(self, event):
@@ -17,8 +26,12 @@ class App:
 
     def on_loop(self):
         pass
+
     def on_render(self):
-        pass
+        self._title = "Pygame Jam 2023"
+        pygame.display.set_caption(self._title)
+        self._display_surf.fill(BLUE)
+        pygame.display.flip()
 
     def on_cleanup(self):
         pygame.quit()
@@ -26,15 +39,15 @@ class App:
     def on_execute(self):
         if self.on_init() == False:
             self._running = False
-        
-        while (self._running):
+        while self._running:
+            self.clock.tick(FPS) 
             for event in pygame.event.get():
                 self.on_event(event)
                 self.on_loop()
                 self.on_render()
         self.on_cleanup()
-                   
- 
-if __name__ == "__main__" :
+
+
+if __name__ == "__main__":
     theApp = App()
     theApp.on_execute()
